86036c88114a5cd718ed9c6b32438f0bab773043
Marco Aurélio <sgtmarcoaurelio@icloud.com>
2024-05-16 21:51:46 -0300

    :sparkles: feat: Adicionar requisição para cadastrar restaurante

---
+++ src/api/register-restaurant.ts
@@ -0,0 +1,16 @@
+import { api } from '@/lib/axios'
+
+interface RegisterRestaurantBody {
+  restaurantName: string
+  managerName: string
+  email: string
+  phone: string
+}
+export async function registerRestaurant({
+  email,
+  restaurantName,
+  managerName,
+  phone,
+}: RegisterRestaurantBody) {
+  await api.post('/restaurants', { email, restaurantName, managerName, phone })
+}
